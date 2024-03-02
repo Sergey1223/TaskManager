c9df2a148249c0328c423c83b718d07cfd493be3
Sergey Mytko <ps2.scph@gmail.com>
2024-03-02 12:20:03 +0300

    [Application] Added database context abstraction.

---
+++ TaskManager.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Entities;
+
+namespace TaskManager.Application.Abstractions
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Domain.Entities.Task> Tasks { get; set; }
+
+        DbSet<TaskList> TaskLists { get; set; }
+
+        DbSet<TaskHistoryItem> TaskHistories { get; set; }
+
+        DbSet<User> Users { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
